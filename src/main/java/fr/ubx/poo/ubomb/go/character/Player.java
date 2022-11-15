4a01bb9e438fbb3be4af3f3696aaf129f2bedc83
kurashyna <lmlucas@outlook.fr>
2022-11-15 10:32:38 +0100

    Rebase

--- src/main/java/fr/ubx/poo/ubomb/go/character/Player.java
+++ src/main/java/fr/ubx/poo/ubomb/go/character/Player.java
@@ -12,9 +12,16 @@ import fr.ubx.poo.ubomb.game.Position;
 import fr.ubx.poo.ubomb.go.GameObject;
 import fr.ubx.poo.ubomb.go.Movable;
 import fr.ubx.poo.ubomb.go.TakeVisitor;
+import fr.ubx.poo.ubomb.go.Walkable;
 import fr.ubx.poo.ubomb.go.decor.Decor;
 import fr.ubx.poo.ubomb.go.decor.bonus.*;
+import fr.ubx.poo.ubomb.launcher.Entity;
 
+import java.util.List;
+
+/**
+ * The type Player.
+ */
 public class Player extends GameObject implements Movable, TakeVisitor {
 
     private Direction direction;
@@ -24,6 +31,7 @@ public class Player extends GameObject implements Movable, TakeVisitor {
     private int bombRange;
     private int bombBag;
 
+
     public Player(Game game, Position position) {
         super(game, position);
         this.direction = Direction.DOWN;
@@ -32,7 +40,11 @@ public class Player extends GameObject implements Movable, TakeVisitor {
         this.bombBag = 1;
     }
 
-
+    /**
+     * Takes a key.
+     *
+     * @param key the key
+     */
     @Override
     public void take(Key key) {
         keys++;
@@ -45,13 +57,37 @@ public class Player extends GameObject implements Movable, TakeVisitor {
         hearth.remove();
     }
 
+    /**
+     * Indique si le déplacement du joueur est possible.
+     * Si le déplacement est possible, on vérifie si le joueur se trouve un bonus
+     * Si le joueur touche un ennemi, il perd une vie
+     *
+     * @param direction La direction du déplacement demandé
+     */
     public void doMove(Direction direction) {
         // This method is called only if the move is possible, do not check again
         Position nextPos = direction.nextPosition(getPosition());
         GameObject next = game.grid().get(nextPos);
-        if (next instanceof Bonus bonus) {
-                bonus.takenBy(this);
+        // Adapter pour récupérer pas que décors
+        if (next != null) {
+            System.out.println("Next : " + next.toString());
         }
+        if (next instanceof Bonus bonus) {
+            bonus.takenBy(this);
+        }
+
+        // TODO: Récupérer le monstre
+
+            /*if ((GameObject) entity instanceof Monster monster) {
+                System.out.println("Monstre touché, vie perdue");
+                this.lives--;
+                if (lives == 0) {
+                    System.out.println("Game Over");
+                } else {
+                    System.out.println("Vies restantes : " + this.getLives());
+                }
+            }*/
+
         setPosition(nextPos);
     }
 
@@ -88,15 +124,17 @@ public class Player extends GameObject implements Movable, TakeVisitor {
         GameObject next = game.grid().get(nextPos);
         boolean isDecor = next instanceof Decor;
         boolean isBonus = next instanceof Bonus;
+        boolean isMonster = next instanceof Monster;
 
-
-        if (nextPos.x() > game.grid().width()-1 || nextPos.x() < 0) {
+        if (nextPos.x() > game.grid().width() - 1 || nextPos.x() < 0) {
             return false;
-        } else if (nextPos.y() > game.grid().height()-1 || nextPos.y() < 0) {
+        } else if (nextPos.y() > game.grid().height() - 1 || nextPos.y() < 0) {
             return false;
         }
-        if(isBonus) {
+        if (isBonus) {
             return true;
+        } else if (isMonster) {
+            return false;
         } else {
             return !isDecor;
         }
