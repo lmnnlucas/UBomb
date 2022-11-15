f48eb0dc5fd8de40d3f05e1e6490e7296aaf9790
kurashyna <lmlucas@outlook.fr>
2022-11-15 11:44:54 +0100

    [Princess.java]: Collision avec Princess = victoire

--- src/main/java/fr/ubx/poo/ubomb/go/character/Player.java
+++ src/main/java/fr/ubx/poo/ubomb/go/character/Player.java
@@ -4,18 +4,14 @@
 
 package fr.ubx.poo.ubomb.go.character;
 
-import fr.ubx.poo.ubomb.engine.StatusBar;
-import fr.ubx.poo.ubomb.engine.Timer;
 import fr.ubx.poo.ubomb.game.Direction;
 import fr.ubx.poo.ubomb.game.Game;
 import fr.ubx.poo.ubomb.game.Position;
 import fr.ubx.poo.ubomb.go.GameObject;
 import fr.ubx.poo.ubomb.go.Movable;
 import fr.ubx.poo.ubomb.go.TakeVisitor;
-import fr.ubx.poo.ubomb.go.Walkable;
 import fr.ubx.poo.ubomb.go.decor.Decor;
 import fr.ubx.poo.ubomb.go.decor.bonus.*;
-import fr.ubx.poo.ubomb.launcher.Entity;
 
 import java.util.List;
 
@@ -31,6 +27,7 @@ public class Player extends GameObject implements Movable, TakeVisitor {
     private int bombRange;
     private int bombBag;
 
+    private boolean haveWon = false;
 
     public Player(Game game, Position position) {
         super(game, position);
@@ -57,6 +54,11 @@ public class Player extends GameObject implements Movable, TakeVisitor {
         hearth.remove();
     }
 
+    @Override
+    public void take(Princess princess) {
+        princess.remove();
+        haveWon = true;
+    }
     /**
      * Indique si le déplacement du joueur est possible.
      * Si le déplacement est possible, on vérifie si le joueur se trouve un bonus
@@ -65,29 +67,22 @@ public class Player extends GameObject implements Movable, TakeVisitor {
      * @param direction La direction du déplacement demandé
      */
     public void doMove(Direction direction) {
+
         // This method is called only if the move is possible, do not check again
         Position nextPos = direction.nextPosition(getPosition());
         GameObject next = game.grid().get(nextPos);
-        // Adapter pour récupérer pas que décors
-        if (next != null) {
-            System.out.println("Next : " + next.toString());
-        }
+
         if (next instanceof Bonus bonus) {
+            System.out.println("Bonus : " + bonus.toString());
             bonus.takenBy(this);
         }
 
-        // TODO: Récupérer le monstre
+        // Si le joueur se trouve sur un ennemi, il perd une vie
         List<GameObject> objectList = game.getGameObjects(nextPos);
         for (GameObject object : objectList) {
             System.out.println("Object : " + object.toString());
             if (object instanceof Monster) {
-                System.out.println("Monstre touché, vie perdue");
                 this.lives--;
-                if (lives == 0) {
-                    System.out.println("Game Over");
-                } else {
-                    System.out.println("Vies restantes : " + this.getLives());
-                }
             }
         }
         setPosition(nextPos);
@@ -154,5 +149,9 @@ public class Player extends GameObject implements Movable, TakeVisitor {
     @Override
     public void explode() {
         // TODO
+    }
+
+    public boolean haveWon(){
+        return haveWon;
     }
 }
--- src/main/java/fr/ubx/poo/ubomb/go/decor/bonus/Princess.java
+++ src/main/java/fr/ubx/poo/ubomb/go/decor/bonus/Princess.java
@@ -1,9 +1,15 @@
 package fr.ubx.poo.ubomb.go.decor.bonus;
 
 import fr.ubx.poo.ubomb.game.Position;
+import fr.ubx.poo.ubomb.go.character.Player;
 
 public class Princess extends Bonus {
     public Princess(Position position) {
         super(position);
+    }
+
+    @Override
+    public void takenBy(Player player) {
+        player.take(this);
     }
 }
